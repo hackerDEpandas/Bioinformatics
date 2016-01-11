8df7642498dd21ae222b830958f432d459a02220
Michael <mikesims321@gmail.com>
2016-01-10 17:56:17 -0800

    update

--- Scripts/transcribe.py
+++ Scripts/transcribe.py
@@ -1,34 +1,11 @@
-class transcribe_DNA(object):
-
-	def __init__(self, dna):
-
-		self.dna = list(dna)
-		self.rna = []
-
-	def generate_RNA(self):
-
-		for i in xrange(len(self.dna)):
-			
-			if self.dna[i] != 'T':
-				self.rna.append(self.dna[i])
-			else:
-				self.rna.append('U')
-
-		return self.rna
-
-
-	def test_DNA(self):
-
-		if list(set(self.dna)) == ['A', 'C', '\n', 'T', 'G']:
-      			return True
-		else:
-      			return False
-
-genome = open('nucleotide.txt')
+from datetime import datetime
+start_time = datetime.now()
+genome = open('rosalind_dna.txt')
 genome_string = genome.read()
-instance = transcribe_DNA(genome_string)
-test_dna = instance.test_DNA()
-
-if test_dna == True:
-	rna_string = instance.generate_RNA()
-	print ''.join(rna_string)
+string = genome_string.replace('\n', '')
+print string
+"""
+def transcribe(string, letter):
+	for i in string:
+		if i != 'T':
+"""
