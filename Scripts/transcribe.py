c08a2cd13eff5f02427aab339ad46abfcf241728
Michael <mikesims321@gmail.com>
2016-01-10 17:59:09 -0800

    Update transcribe.py

--- Scripts/transcribe.py
+++ Scripts/transcribe.py
@@ -3,15 +3,15 @@ import numpy as np
 start_time = datetime.now()
 genome = open('rosalind_rna.txt')
 genome_string = genome.read()
-rna = []
+dna = []
 def transcribe(string):
 	string = string.replace('\n', '')
 	for i in string:
 		if i != 'T':
-			rna.append(i)
+			dna.append(i)
 		else:
-			rna.append('U')
-	return ''.join(rna)
+			dna.append('U')
+	return ''.join(dna)
 
 saved = transcribe(genome_string)
 print saved
