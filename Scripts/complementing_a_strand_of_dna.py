d93df5041dcb00d101ce2e37f55e0e5fb407edf9
Michael <mikesims321@gmail.com>
2016-01-15 14:15:06 -0800

    update complementing_a_strand_of_dna

---
+++ Scripts/complementing_a_strand_of_dna.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+start_time = datetime.now()
+genome = open('rosalind_revc.txt')
+genome_string = genome.read()
+rev_comp = []
+
+string = genome_string.replace('\n', '')
+for i in reversed(string):
+	if i == 'A':
+		rev_comp.append('T')
+	elif i == 'T':
+		rev_comp.append('A')
+	elif i == 'C':
+		rev_comp.append('G')
+	else:
+		rev_comp.append('C')
+
+print ''.join(rev_comp)	
+
